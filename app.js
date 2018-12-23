f8138456780ea20929185f701df4208500b0bbe3
anjalicprs <anjalicprs@gmail.com>
2018-12-23 22:18:48 +0530

    remove changes

--- app.js
+++ app.js
@@ -5,16 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var bcrypt = require('bcryptjs');
-// var models = require('./models');
-var sequelize = require('sequelize');
-//var Admins = models.Admins;
-
-//var index = require('./routes/index');
-//var register = require('./routes/register');
-//var admin = require('./routes/admin');
-
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -27,7 +18,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', index);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
